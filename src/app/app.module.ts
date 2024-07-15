5ea48f5c35ad57bb7a3d39a009a85caee6efc49c
sandeepbehra <sandeepbehra091998@gmail.com>
2024-07-15 12:24:34 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 
 
+
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
     
   ],
   imports: [
